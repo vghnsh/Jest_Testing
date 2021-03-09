b140c0dd2f9557e1f5f8506af03fa5b4bbc8c9a2
vghnsh <vighneshchaurasia17@gmail.com>
2021-03-09 18:08:04 +0530

    First test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,42 @@
 import { render, screen } from '@testing-library/react';
+import { shallow,mount } from 'enzyme';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe ('Basic test Cases',() =>{
+
+  {/*
+    ***********React testing*************
+    test('renders learn react link', () => {
+      const {getByText}= render(<App />);
+      const linkElement = getByText('This is basic Test');
+      expect(linkElement).toBeInTheDocument();
+  })
+  */}
+
+  // Before every enzyme test we need 
+  let wrapper;
+  beforeEach(()=>{
+    wrapper = shallow(<App/>);
+  })
+
+  //Testing using Enzyme
+  test('renders learn react link', () => {
+    expect(wrapper.find("h1").text()).toContain('This is basic Test')
+  });
+
+  {/*
+    ***********React testing*************
+     test('renders button', () => {
+       const {getByTestId}= render(<App />);
+       const linkElement = getByTestId('btn');
+       expect(linkElement).toBeInTheDocument();
+  })
+  */}
+
+  //Testing Button using Enzyme
+  test('Testing A button', () => {
+      expect(wrapper.find("#btn").text()).toBeTruthy();
+  });
+  
 });
+
